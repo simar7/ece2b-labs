85778a2990e33978f10719e3dea8ed083626100b
Simarpreet Singh <simar@linux.com>
2013-09-17 20:20:25 -0400

    Fixed the user id and group id issue.
    No clue what the issue was, the only resolution I could
    figure out was to copy all the contents into a new file
    and compile that.
    
    Note to self: Never touch files on a MAC machine. They do
    add some metadata to it.
    
    Note to self: K.I.S.S.
    
    Sigh.
    
    Signed-off-by: Simarpreet Singh <simar@linux.com>

---
+++ s244sing/ECE254-Lab0/folderofdoom/simple_ls.c
@@ -0,0 +1,181 @@
+#include <unistd.h>
+#include <sys/stat.h>
+#include <stdlib.h> 	// For exit()
+#include <dirent.h>	// For opendir()
+#include <stdio.h>
+#include <sys/types.h>	// For opendir()
+#include <time.h>	// For ctime()
+#include <grp.h>	// For getgrgid()
+#include <errno.h>
+#include <string.h>
+/*
+int filepermissions(char* str_path)
+{
+	char str[] = "---------";
+	struct stat buf;
+
+    if( lstat(str_path, &buf) < 0)
+	{	
+        perror("lstat failed on file\n");
+        return errno;
+    }	
+    
+    mode_t mode = buf.st_mode;
+	str[0] = (mode & S_IRUSR) ? 'r' : '-';
+    str[1] = (mode & S_IWUSR) ? 'w' : '-';
+	str[2] = (mode & S_IXUSR) ? 'x' : '-';
+	str[3] = (mode & S_IRUSR) ? 'r' : '-';
+    str[4] = (mode & S_IWGRP) ? 'w' : '-';
+	str[5] = (mode & S_IXGRP) ? 'x' : '-';
+	str[6] = (mode & S_IROTH) ? 'r' : '-';
+    str[7] = (mode & S_IWOTH) ? 'w' : '-';
+	str[8] = (mode & S_IXOTH) ? 'x' : '-';
+
+	printf("%s\t", str);
+
+	char* ptr;
+    if( S_ISREG(buf.st_mode)) 	ptr = "regular file";
+	else if (S_ISDIR(buf.st_mode))  ptr = "directory";
+	else if (S_ISCHR(buf.st_mode))  ptr = "character special";
+	else if (S_ISBLK(buf.st_mode))  ptr = "block special";
+	else if (S_ISFIFO(buf.st_mode)) ptr = "fifo";
+#ifdef S_ISLNK
+	else if (S_ISLNK(buf.st_mode))  ptr = "symbolic link";
+#endif
+#ifdef S_ISSOCK
+	else if (S_ISSOCK(buf.st_mode)) ptr = "socket";
+#endif
+	else    
+        ptr = "unknown file bro :(";
+	
+	printf("%s\t", ptr);
+    return 0;
+}
+*/
+
+/*
+int filesizes(char* str_path)
+{
+    struct stat buf;
+    if (lstat(str_path, &buf) < 0)
+    {
+        perror("stat() failed");
+        return errno;
+    }
+
+    off_t filesize = buf.st_size;
+    printf("%5llub\t", (long long unsigned)filesize);
+    printf("%s\n", str_path);
+
+    return 0;
+}
+
+int filetimes(char* str_path)
+{
+
+    struct stat buf;
+	if (lstat(str_path, &buf) < 0)
+	{
+		perror("lstat failed");
+	    return errno;
+    }
+	
+	time_t accesstime = buf.st_atime;
+	char* timeptr = ctime(&accesstime);
+	
+    printf("Access Time: %s", timeptr);
+
+    time_t modifytime = buf.st_mtime;
+	timeptr = ctime(&modifytime);
+	printf("Modify Time: %s", timeptr);
+		
+	time_t statuschangetime = buf.st_ctime;
+	timeptr = ctime(&statuschangetime);
+	printf("Status Time: %s", timeptr);
+
+    return 0;
+}
+*/
+
+int fileownership(char* str_path)
+{
+	struct stat buf;
+    if (stat(str_path, &buf ) < 0 )
+	{ 	
+		perror("stat() failed");
+		return -1;
+	}
+
+	gid_t grpid = buf.st_gid;
+    uid_t usrid = buf.st_uid;
+
+    printf("%5d\t", grpid);
+    printf("%5d\t", usrid);
+
+	struct passwd* uid_ptr;
+	uid_ptr = getpwuid(buf.st_uid);
+
+	printf("%s", uid_ptr->pw_name);
+	/*
+	struct passwd* uid_struct;
+	uid_struct = getpwuid(buf.st_uid);
+
+	struct group* gid_struct;
+	gid_struct = getgrgid(buf.st_gid);
+
+	printf("GID: %s\n", gid_struct->gr_name);
+    printf("UID: %s\n", uid_struct->pw_name);
+   */
+	return 0;
+
+}
+int main( int argc, char* argv[] )
+{
+	DIR *p_dir;
+	struct dirent *p_dirent;
+
+    if( (argc < 2) || ( strcmp( argv[1], "-h") == 0 )  || (argc > 3) )
+    {
+        printf("Usage: %s <dir name>\n", argv[0]);
+        printf("-h \t Show help\n");
+        printf("-vvv \t Be Very Verbose (default)\n");
+        printf("-P \t Be Precise\n");
+        exit(1);
+    }
+
+	if( (p_dir = opendir(argv[1])) == NULL )
+	{
+		printf("opendir(%s) failed\n", argv[1]);
+		exit(1);
+	}
+
+	while( (p_dirent = readdir(p_dir)) != NULL )
+	{
+		char *str_path = p_dirent->d_name;
+
+		if ( str_path == NULL )	
+		{
+    		printf("Null pointer found!\n");
+			exit(2);
+		}
+
+/*
+    if( filepermissions(str_path) != 0 )	
+*/		printf("Something went wrong! Errno: %d\n", errno);
+
+    if( fileownership(str_path) != 0 )
+        printf("Something went wrong! Errno: %d\n", errno);
+/*
+    if( filesizes(str_path) != 0 )
+        printf("Something went wrong! Errno: %d\n", errno);
+
+    if( ! ((argv[2]) && strcmp(argv[2],  "-p")) )
+        if( filetimes(str_path) != 0 )
+            printf("[filetimes]: Something went wrong! Errno: %d\n", errno);
+    
+	*/   }
+    
+    closedir(p_dir);
+
+    return 0;
+}
