3c3670b13fd30a3876fe07b8f225214566d188b7
Simarpreet Singh <simar@linux.com>
2013-11-06 11:37:06 -0500

    Adding producer(), still untested
    Signed-off-by: Simarpreet Singh <simar@linux.com>

--- ECE254/s244sing/ECE254-Lab2/lab2/linux/lab/producer.c
+++ ECE254/s244sing/ECE254-Lab2/lab2/linux/lab/producer.c
@@ -22,16 +22,10 @@ int producer( int N, int B )
 	attr.mq_msgsize = sizeof(int);
 	attr.mq_flags = 0; 	// Blocking queue activate.
 
-	qdes = mq_open( qname, qdes_mode, mode, &attr );
-	if( qdes == -1 )
-	{
-		printf("[producer]: mq_open() failed!\t ERRNO: %d\n", errno);
-		exit(1);
-	}
 	
 	if( gettimeofday( &time_a, NULL ) == -1 )
 	{
-		printf("[producer]: gettimeofday() failed!\t ERRNO: %d\n", errno);
+		printf("[producer]: gettimeofday(ime_a) failed!\t ERRNO: %d\n", errno);
 		exit(1);
 	}
 
@@ -43,6 +37,54 @@ int producer( int N, int B )
 		exit(1);
 	}
 	
+	qdes = mq_open( qname, qdes_mode, mode, &attr );
+	if( qdes == -1 )
+	{
+		printf("[producer]: mq_open() failed!\t ERRNO: %d\n", errno);
+		exit(1);
+	}
+
+	if( gettimeofday( &time_b, NULL ) == -1 )
+	{
+		printf("[producer]: gettimeofday(time_b) failed!\t ERRNO: %d\n", errno);
+		exit(1);
+	}
+
+	int msg_count = 0;
+	int random_num = 0;
+
+	while( msg_count < N )
+	{
+		random_num = rand();
+		if( mq_send( qdes, (char *)random_num, sizeof(random_num), 0) == -1 )
+			printf("[producer]: mq_send failed!\t ERRNO: %d", errno);
+		else
+			printf("[producer]: mq_send worked!\t Tx: %d", random_num);
+		msg_count++;
+	}	
+
+	printf("[producer]: Tx finished!\n");
+
+	if( gettimeofday( &time_c, NULL ) == -1 )
+	{
+		printf("[producer]: gettimeofday(time_c) failed!\t ERRNO: %d\n", errno);
+		exit(1);
+	}
+
+	if( mq_close(qdes) == -1 )
+	{
+		printf("[producer]: mq_close() failed!\t ERRNO: %d\n", errno);
+		exit(1);
+	}
+	
+	if( mq_unlink(qname) != 0 )
+	{
+		printf("[producer]: mq_unlink() failed\t ERRNO: %d\n", errno);
+		exit(1);
+	}
+
+	return 0;
+}	
 
 int main( int argc, char* argv[] )
 {
