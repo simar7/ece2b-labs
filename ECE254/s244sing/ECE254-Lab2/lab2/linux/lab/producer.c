9d981a85f616b8f4437ce0c88c24f2672a84ae7a
Simarpreet Singh <simar@linux.com>
2013-11-06 15:39:52 -0500

    Almost done, adding timing func. later
    Signed-off-by: Simarpreet Singh <simar@linux.com>

--- ECE254/s244sing/ECE254-Lab2/lab2/linux/lab/producer.c
+++ ECE254/s244sing/ECE254-Lab2/lab2/linux/lab/producer.c
@@ -14,7 +14,7 @@ struct timeval time_a, time_b, time_c;
 int producer( int N, int B )
 {
 	mqd_t qdes;
-	char qname[] = "/mailbox_s244sing_lab2_producer";
+	char qname[] = "/mailbox_s244sing";
 	mode_t mode = S_IRUSR | S_IWUSR;
 	int qdes_mode = O_RDWR | O_CREAT;
 	struct mq_attr attr;
@@ -26,7 +26,7 @@ int producer( int N, int B )
 	
 	if( gettimeofday( &time_a, NULL ) == -1 )
 	{
-		printf("[producer]: gettimeofday(ime_a) failed!\t ERRNO: %d\n", errno);
+		printf("[producer]: gettimeofday(time_a) failed!\t ERRNO: %d\n", errno);
 		exit(1);
 	}
 
@@ -38,59 +38,90 @@ int producer( int N, int B )
 		exit(1);
 	}
 	
-	qdes = mq_open( qname, O_RDWR | O_CREAT , mode, &attr );
-	if( qdes == -1 )
+	else if( child_pid != 0)	// This is the parent.	
 	{
-		printf("[producer]: mq_open() failed!\t ERRNO: %d\n", errno);
-		exit(1);
+		qdes = mq_open(qname, O_RDWR | O_CREAT, mode, &attr);
+		if( qdes == -1 )
+		{
+			printf("[producer]: mq_open() failed!\t ERRNO: %d\n", errno);
+			exit(1);
+		}
+
+		if( gettimeofday( &time_b, NULL ) == -1 )
+		{
+			printf("[producer]: gettimeofday(time_b) failed!\t ERRNO: %d\n", errno);
+			exit(1);
+		}
+
+		int msg_count = 0;
+		int random_num = 0;
+
+			while( msg_count < N )
+			{
+				random_num = (rand() % 9);
+				if( mq_send( qdes, (char *)&random_num, sizeof(random_num), 0) == -1 )
+					printf("[producer]: mq_send failed!\t ERRNO: %d\n", errno);
+				else
+					printf("[producer]: mq_send worked!\t Tx: %d\n", random_num);
+				msg_count++;
+			}	
+
+			printf("[producer]: Tx finished!\n");
+
+			if( gettimeofday( &time_c, NULL ) == -1 )
+			{
+				printf("[producer]: gettimeofday(time_c) failed!\t ERRNO: %d\n", errno);
+				exit(1);
+			}
+
+			if( mq_close(qdes) == -1 )
+			{
+				printf("[producer]: mq_close() failed!\t ERRNO: %d\n", errno);
+				exit(1);
+			}
+			
+			if( mq_unlink(qname) != 0 )
+			{
+				printf("[producer]: mq_unlink() failed\t ERRNO: %d\n", errno);
+				exit(1);
+			}
+
 	}
-
-	if( gettimeofday( &time_b, NULL ) == -1 )
+	else
 	{
-		printf("[producer]: gettimeofday(time_b) failed!\t ERRNO: %d\n", errno);
-		exit(1);
-	}
+		qdes = mq_open(qname, O_RDWR | O_CREAT, mode, &attr);
+		if( qdes == -1 )
+		{
+			printf("[consumer]: mq_open() failed!\t ERRNO: %d\n", errno);
+			exit(1);
+		}
 
-	int msg_count = 0;
-	int random_num = 0;
-
-	while( msg_count < N )
-	{
-		random_num = rand();
-		if( mq_send( qdes, (char *)random_num, sizeof(random_num), 0) == -1 )
-			printf("[producer]: mq_send failed!\t ERRNO: %d", errno);
-		else
-			printf("[producer]: mq_send worked!\t Tx: %d", random_num);
-		msg_count++;
-	}	
-
-	printf("[producer]: Tx finished!\n");
-
-	if( gettimeofday( &time_c, NULL ) == -1 )
-	{
-		printf("[producer]: gettimeofday(time_c) failed!\t ERRNO: %d\n", errno);
-		exit(1);
-	}
-
-	if( mq_close(qdes) == -1 )
-	{
-		printf("[producer]: mq_close() failed!\t ERRNO: %d\n", errno);
-		exit(1);
+		int msg_count = 0;
+		int random_num = 0;
+		while( msg_count < B )
+		{
+			if( mq_receive (qdes, (char*)&random_num, sizeof(int), 0) == -1 )
+				printf("[consumer]: mq_receive failed!\t ERRNO: %d\n", errno);
+			else
+				printf("[consumer]: mq_receive worked!\t Tx: %d\n", random_num);
+			msg_count++;
+		}	
+		
+		if( mq_close(qdes) == -1 )
+		{
+			printf("[consumer]: mq_close() failed!\t ERRNO: %d\n", errno);
+			exit(1);
+		}
+		
 	}
 	
-	if( mq_unlink(qname) != 0 )
-	{
-		printf("[producer]: mq_unlink() failed\t ERRNO: %d\n", errno);
-		exit(1);
-	}
-
-	return 0;
+		return 0;
 }	
 
 int main( int argc, char* argv[] )
 {
 	int return_val = 0;
-	return_val = producer( (int) argv[1], (int) argv[2] );
+	return_val = producer( atoi(argv[1]), atoi(argv[2]) );
 
 	if( return_val != 0 )
 	{
